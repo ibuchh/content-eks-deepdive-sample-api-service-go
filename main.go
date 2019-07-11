e35ebbfcb6f59b2bedc9488ec824556da5d98247
Irshad A Buchh <ibuchh@gmail.com>
2019-07-11 19:34:14 -0400

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "SA Launch Presentation Today !"}
+		res := &response{Message: "SA Launch Presentation !"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
