a71b45630c01c734b14abba5702fa9c164a7b785
Irshad A Buchh <ibuchh@gmail.com>
2019-07-11 18:03:06 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,29 +1,53 @@
 package main
 
 import (
-	"html/template"
+	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
+	"os"
+	"strings"
 )
 
-type Todo struct {
-	Title string
-	Done  bool
-}
-
-type TodoPageData struct {
-	PageTitle string
-	Todos     []Todo
-}
-
 func main() {
-	tmpl := template.Must(template.ParseFiles("index.html"))
-
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		data := TodoPageData{
-			PageTitle: "Team Members",
-		}
-		tmpl.Execute(w, data)
-	})
 
+		f := fib()
+
+		res := &response{Message: "Hello World"}
+
+		for _, e := range os.Environ() {
+			pair := strings.Split(e, "=")
+			res.EnvVars = append(res.EnvVars, pair[0]+"="+pair[1])
+		}
+
+		for i := 1; i <= 90; i++ {
+			res.Fib = append(res.Fib, f())
+		}
+
+		// Beautify the JSON output
+		out, _ := json.MarshalIndent(res, "", "  ")
+
+		// Normally this would be application/json, but we don't want to prompt downloads
+		w.Header().Set("Content-Type", "text/plain")
+
+		io.WriteString(w, string(out))
+
+		fmt.Println("Hello world - the log message")
+	})
 	http.ListenAndServe(":8080", nil)
+}
+
+type response struct {
+	Message string   `json:"message"`
+	EnvVars []string `json:"env"`
+	Fib     []int    `json:"fib"`
+}
+
+func fib() func() int {
+	a, b := 0, 1
+	return func() int {
+		a, b = b, a+b
+		return a
+	}
 }
